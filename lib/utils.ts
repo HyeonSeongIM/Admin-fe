4b3fa8cfa8e0b4736e5167e8218b8462ecf5bca3
HyeonSeong <gus8131@naver.com>
2025-06-10 16:24:33 +0900

    tawilnd and shadcn

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Class와 Name을 줄인 말 = cn
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
